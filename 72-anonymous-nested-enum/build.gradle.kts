6b58f1486d6a0fcd0b53c0e3e6dab1baf672051f
DanySK <danilo.pianini@gmail.com>
2025-03-18 10:05:00 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@3960b7c014b372653fb5a1139103ef130d39bea2 🚀

--- 72-anonymous-nested-enum/build.gradle.kts
+++ 72-anonymous-nested-enum/build.gradle.kts
@@ -8,9 +8,12 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.1")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.1")
+    // The BOM (Bill of Materials) synchronizes all the versions of Junit coherently.
+    testImplementation(platform("org.junit:junit-bom:5.12.1"))
+    // The annotations, assertions and other elements we want to have access when compiling our tests.
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    // The engine that must be available at runtime to run the tests.
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
